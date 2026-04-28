5c5941ae7a11ae1d4eace4708177067df630bef7
dotbillu <billudestroyer@outlook.com>
2026-04-29 01:55:19 +0530

    zoom controll on right

--- apps/web/app/explore/components/Map.tsx
+++ apps/web/app/explore/components/Map.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  ZoomControl,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default function Map() {
@@ -9,8 +15,10 @@ export default function Map() {
       center={[51.505, -0.09]}
       zoom={13}
       scrollWheelZoom={false}
+      zoomControl={false}
       style={{ height: "100vh", width: "100%" }}
     >
+      <ZoomControl position="topright" />
       <TileLayer
         attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
